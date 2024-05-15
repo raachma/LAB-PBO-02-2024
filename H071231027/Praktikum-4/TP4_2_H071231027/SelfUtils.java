f6f9ccbde57a9242eb8df845e7a32753222e4628
Kevinardhana096 <165297553+Kevinardhana096@users.noreply.github.com>
2024-05-15 15:32:43 +0800

    Delete H071231027 directory

--- H071231027/Praktikum-4/TP4_2_H071231027/SelfUtils.java
+++
@@ -1,8 +0,0 @@
-package TP4_2_H071231027;
-
-public class SelfUtils {
-    public static void displaySelfData() {
-        System.out.println("Nama : Kevin");
-        System.out.println("NIM  :H071231027");
-    }
-}
